de3fc92e7c7d9c9dd4fa63b33b919996df1abf0e
franciscocamellon <franciscocamellon@gmail.com>
2021-08-15 17:25:25 -0300

    Finished

---
+++ tp06/SocialNetwork.Web/Controllers/LayoutController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult StarterPage()
+        {
+            return View();
+        }
+
+        public IActionResult FixedFooter()
+        {
+            return View();
+        }
+
+        public IActionResult FullHeight()
+        {
+            return View();
+        }
+
+        public IActionResult FullWidth()
+        {
+            return View();
+        }
+
+        public IActionResult BoxedLayout()
+        {
+            return View();
+        }
+
+        public IActionResult MinifiedSidebar()
+        {
+            return View();
+        }
+    }
+}
