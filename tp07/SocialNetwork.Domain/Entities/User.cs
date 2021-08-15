af9c10f00f8eeb067f0f62c88ba813524aa94b74
franciscocamellon <franciscocamellon@gmail.com>
2021-08-15 17:33:54 -0300

    Tp07 Initial

---
+++ tp07/SocialNetwork.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SocialNetwork.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ tp07/SocialNetwork.Web/Controllers/FeedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class FeedController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
