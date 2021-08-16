3fb28a62bd863ce564ebaa46f6e91d07013bfe0a
franciscocamellon <franciscocamellon@gmail.com>
2021-08-15 23:43:03 -0300

    Finished

---
+++ tp07/SocialNetwork.Domain/Entities/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.Domain.Entities
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ tp07/SocialNetwork.Web/Controllers/ProfilesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Data;
+using SocialNetwork.Domain.Entities;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Profiles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Profile.ToListAsync());
+        }
+
+        // GET: Profiles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profile
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,UserId")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(profile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profile.FindAsync(id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,UserId")] Profile profile)
+        {
+            if (id != profile.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(profile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfileExists(profile.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await _context.Profile
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var profile = await _context.Profile.FindAsync(id);
+            _context.Profile.Remove(profile);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfileExists(int id)
+        {
+            return _context.Profile.Any(e => e.Id == id);
+        }
+    }
+}
